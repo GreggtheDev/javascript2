d65fd967f4fa8c632671d433c0a92fde9451c02d
GreggtheDev <greggthedev@gmail.com>
2024-06-18 11:33:40 -0400

    joined all elements of array into a string with a specified separator

---
+++ array-methods-2.js
@@ -0,0 +1,10 @@
+// 1. Joining Array Elements
+/**
+ * This function joins all elements of an array into a string with a specified separator.
+ * @param {Array} arr - An array of strings.
+ * @param {string} separator - A separator string.
+ * @returns {string} - A string composed of array elements separated by the given separator.
+ */
+function joinElements(arr, separator) {
+    return arr.join(separator);
+}
