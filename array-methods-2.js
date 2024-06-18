b6c9cc450d9cbcbded33a746c97d4529e531c63c
GreggtheDev <greggthedev@gmail.com>
2024-06-18 11:44:06 -0400

    called each function

--- array-methods-2.js
+++ array-methods-2.js
@@ -50,3 +50,18 @@ function unionArrays(arr1, arr2) {
     let union = [...new Set([...arr1, ...arr2])];
     return union.sort((a, b) => a - b);
 }
+
+// Call joinElements function
+console.log(joinElements(["Red", "Green", "White", "Black"], ",")); // Output: "Red,Green,White,Black"
+
+// Call sortArray function
+console.log(sortArray([3, 1, 4, 1, 5, 9, 2, 6])); // Output: [1, 1, 2, 3, 4, 5, 6, 9]
+
+// Call flattenArray function
+console.log(flattenArray([1, [2, [3, [4]], 5]])); // Output: [1, 2, 3, 4, 5]
+
+// Call filterValues function
+console.log(filterValues([58, '', 'abcd', true, null, false, 0])); // Output: [58, 'abcd', true]
+
+// Call unionArrays function
+console.log(unionArrays([1, 2, 3], [100, 2, 1, 10])); // Output: [1, 2, 3, 10, 100]
