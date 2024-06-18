3d9c32656ff15ec6e5656becd7be79cc074cfea7
GreggtheDev <greggthedev@gmail.com>
2024-06-18 09:11:01 -0400

    added previous photo method to decrease current photo index by 1 if there is a previous photo

--- photo-slideshow.js
+++ photo-slideshow.js
@@ -8,3 +8,10 @@ let photoSlideshow = {// Declaration of main object
             console.log('End of the slideshow');// Log the end of the slideshow
         }
     },
+    prevPhoto: function() {// Method for changing the current photo to the previous photo
+        if(this.currrentPhotoIndex > 0) {// If the current photo is not the first photo
+            this.currentPhotoIndex--;// Decrement the current photo index
+        } else {// If the current photo is the first photo
+            console.log('Start of the slideshow');// Log the beginning of the slideshow
+        }
+    },
