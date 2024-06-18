01e4ea56b2633e5a8bda316d80bb24571c82ff48
GreggtheDev <greggthedev@gmail.com>
2024-06-18 08:58:19 -0400

    added property for the current index of photos

--- photo-slideshow.js
+++ photo-slideshow.js
@@ -1,3 +1,3 @@
 let photoSlideshow = {// Declaration of main object
     photoList: ['photo1', 'photo2', 'photo3', 'photo4', 'photo5', 'photo6'],// Property with array of photos
-    }
+    currentPhotoIndex: 0,// Property with the index of the current photo}
