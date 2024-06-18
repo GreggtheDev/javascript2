f32ee3b150b9165aefa427b088fae6d09b9ce609
GreggtheDev <greggthedev@gmail.com>
2024-06-18 09:06:44 -0400

    added nextPhoto method to increase currenPhotoIndex by 1 i there's a next photo in the slideshow

--- photo-slideshow.js
+++ photo-slideshow.js
@@ -1,3 +1,10 @@
 let photoSlideshow = {// Declaration of main object
     photoList: ['photo1', 'photo2', 'photo3', 'photo4', 'photo5', 'photo6'],// Property with array of photos
-    currentPhotoIndex: 0,// Property with the index of the current photo}
+    currentPhotoIndex: 0,// Property with the index of the current photo
+    nextPhoto: function() {// Method for changing the current photo to the next photo
+        if(this.currrentPhotoIndex < this.photoList.length - 1) {// If the current photo is not the last photo
+            this.currentPhotoIndex++;// Increment the current photo index
+        } else {// If the current photo is the last photo
+            console.log('End of the slideshow');// Log the end of the slideshow
+        }
+    },
