aab5711a169189f13ac18ebc2c73a0348cde2bb4
GreggtheDev <greggthedev@gmail.com>
2024-07-01 10:08:01 -0400

    Update array-methods.js
    added comments

--- array-methods.js
+++ array-methods.js
@@ -1,33 +1,41 @@
 // Data set
 const users = [
-    {id: 1232, firstName: 'Cam', lastName: 'Danvers', logins: 35, isPremiumMember: false},
-    {id: 3283, firstName: 'Elijah', lastName: 'Hawkings', logins: 3, isPremiumMember: true},
-    {id: 9283, firstName: 'Ragupathy', lastName: 'Bodem', logins: 12, isPremiumMember: false},
-    {id: 6972, firstName: 'Jamilla', lastName: 'Johnson', logins: 5, isPremiumMember: true},
-    {id: 3998, firstName: 'Jose', lastName: 'Rivera', logins: 16, isPremiumMember: false},
-    {id: 4982, firstName: 'Ted', lastName: 'Witherspoon', logins: 53, isPremiumMember: true},
-    {id: 6929, firstName: 'Igor', lastName: 'Silenski', logins: 23, isPremiumMember: false},
-    {id: 3879, firstName: 'Ira', lastName: 'Bolislovitz', logins: 9, isPremiumMember: false}
+    // This is an array of user objects. Each object represents a user with properties like id, firstName, lastName, logins, and isPremiumMember.
 ];
 
 // Find a user named 'Jose'
 let jose = users.find(user => user.firstName === 'Jose');
+// This line uses the find method to get the first user object in the users array where the firstName property is 'Jose'.
+
 console.log(jose);
+// This line logs the user object for 'Jose' to the console.
 
 // Get an array of all the premium members
 let premiumMembers = users.filter(user => user.isPremiumMember);
+// This line uses the filter method to create a new array with all users who are premium members (isPremiumMember property is true).
+
 console.log(premiumMembers);
+// This line logs the array of premium member user objects to the console.
 
 // Get an array of all the user last names
 let lastNames = users.map(user => user.lastName);
+// This line uses the map method to create a new array with the lastName of each user in the users array.
+
 console.log(lastNames);
+// This line logs the array of user last names to the console.
 
 // Get an array of the full names who have logged in more than 10 times
 let frequentUsers = users
     .filter(user => user.logins > 10)
     .map(user => `${user.firstName} ${user.lastName}`);
+// This line first filters the users who have logged in more than 10 times, then maps those users to an array of their full names.
+
 console.log(frequentUsers);
+// This line logs the array of frequent user full names to the console.
 
 // Get the total number of logins for the list of users
 let totalLogins = users.reduce((total, user) => total + user.logins, 0);
+// This line uses the reduce method to get the total number of logins from all users. It starts with a total of 0 and adds the number of logins for each user.
+
 console.log(totalLogins);
+// This line logs the total number of logins to the console.
